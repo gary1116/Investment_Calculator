9d7c3348371258b15c8c429858dbd326e54c111c
gary1116 <garygodwin234@gmail.com>
2024-08-01 15:47:42 +0530

    added base files updated bg, added logo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FloatingImage from './components/FloatingImage/FloatingImage';
+function App() {
+  return (
+    <div className="App fullscreen-element">
+      <p>Hii</p>
+
+      <FloatingImage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FloatingImage/FloatingImage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import money from '../../assets/flying-money.png';
+import '../FloatingImage/FloatingImage.css';
+
+function FloatingImage() {
+  return (
+    <div className='floater'>
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+      <img src={money} alt="money Image" />
+
+    </div>
+  )
+}
+
+export default FloatingImage;
